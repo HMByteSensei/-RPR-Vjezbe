b2980dfc6ad3fe751d92e1d5ba574d698464f966
HMByteSensei <127348494+HMByteSensei@users.noreply.github.com>
2023-12-10 15:44:28 +0100

    Finall commit

--- lab7z1/src/main/java/ba/unsa/etf/rpr/tutorial7/lab7z1/z1Application.java
+++ lab7z1/src/main/java/ba/unsa/etf/rpr/tutorial7/lab7z1/z1Application.java
@@ -12,10 +12,14 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class z1Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(z1Application.class.getResource("/fxml/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(z1Application.class.getResource("/fxml/z1view.fxml"));
+//        z1Controller controller = new z1Controller();
+//        fxmlLoader.setController(controller);
+//        Parent root = fxmlLoader.load();
 //        z1Controller controller = new z1Controller();
 //        fxmlLoader.setController(controller);
         Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        //Scene scene = new Scene(root);
         stage.setTitle("Korisnici");
         stage.setScene(scene);
         stage.setResizable(false);
